1e99b8d821b154fe5afec914af1dc9a607348ded
agent green <a.2000.g1@yandex.ru>
2024-05-01 18:24:48 +0300

    Задание спринта 5 выполнено

---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Алиса  Грищенкова on 21.04.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion()
+}
